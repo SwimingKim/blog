8564149ffde1cbc77bb0e5283bfe0559b2c0b270
skim <rumex13@naver.com>
2023-07-30 20:26:32 +0900

    resolve error

--- components/Layout/Article/Article.tsx
+++ components/Layout/Article/Article.tsx
@@ -126,30 +126,6 @@ export default function Article() {
       Equation,
       Pdf,
       Modal,
-      pageLink: ({
-        href,
-        as,
-        passHref,
-        prefetch,
-        replace,
-        scroll,
-        shallow,
-        locale,
-        ...props
-      }) => (
-        <Link
-          href={href}
-          as={as}
-          passHref={passHref}
-          prefetch={prefetch}
-          replace={replace}
-          scroll={scroll}
-          shallow={shallow}
-          locale={locale}
-        >
-          <a {...props} />
-        </Link>
-      ),
     }),
     []
   )
